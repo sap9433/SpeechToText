ed883ce8c1488c72b405927cb3eafed293d6035a
Saptarshi Chatterjee <diesel@falcon.local>
2018-11-30 18:03:17 -0600

    Revolutionary

--- src/main/java/iit/talon/KafKaConsumer.java
+++ src/main/java/iit/talon/KafKaConsumer.java
@@ -1,14 +1,25 @@
 package iit.talon;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import iit.talon.service.GetText;
+
 @Component
 public class KafKaConsumer {
+    @Autowired
+    GetText cloudService;
+
     private static final Logger logger = LoggerFactory.getLogger(KafKaConsumer.class);
    @KafkaListener(topics = "test", groupId = "group-id")
     public void listen(String message) {
-        logger.info("Received Messasge in group - group-id: " + message);
+        logger.info("Received Messasge: " + message);
+    }
+
+    @KafkaListener(topics = "recievedsound")
+    public void processSound(String message) {
+        logger.info(cloudService.transcript());
     }
 }
