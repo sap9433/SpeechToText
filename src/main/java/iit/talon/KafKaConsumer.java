dace8488600b4f906363f66ac9665584aff1ee24
Saptarshi Chatterjee <diesel@falcon.local>
2018-12-02 19:28:08 -0600

    It works

--- src/main/java/iit/talon/KafKaConsumer.java
+++ src/main/java/iit/talon/KafKaConsumer.java
@@ -3,6 +3,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 import iit.talon.service.GetText;
@@ -12,15 +13,19 @@ public class KafKaConsumer {
     @Autowired
     GetText cloudService;
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
     private static final Logger logger = LoggerFactory.getLogger(KafKaConsumer.class);
    @KafkaListener(topics = "test", groupId = "group-id")
     public void listen(String message) {
-        //logger.info("Received Messasge: " + message);
+        logger.info("Received Messasge: " + message);
     }
 
     @KafkaListener(topics = "recievedsound1")
     public void processSound(byte[] sounddata) {
-        logger.info(cloudService.transcript(sounddata));
-        //logger.info("### KYA HAI YEHHH ### ");
+        String transcript = cloudService.transcript(sounddata);
+        logger.info(transcript);
+        kafkaTemplate.send("test", transcript);
     }
 }
