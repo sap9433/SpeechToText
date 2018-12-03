76618f4d3c12de42fce5f1cc5e7f715f47c43fd1
Saptarshi Chatterjee <diesel@falcon.local>
2018-12-03 16:48:22 -0600

    Works Boy

--- src/main/java/iit/talon/service/KafKaConsumer.java
+++ src/main/java/iit/talon/service/KafKaConsumer.java
@@ -5,9 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 import iit.talon.service.GetText;
